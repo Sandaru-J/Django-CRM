9a87e3a1216afdfec7fc63cf434edaca9d955abb
Sandaru Jayathilaka <sandarumanga2000@gmail.com>
2024-08-22 10:24:46 +0530

    Delete Backend directory

--- Backend/products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ProductControl
-
-urlpatterns = [
-    path('product/',ProductControl.as_view()),
-    path('product/<int:id>/',ProductControl.as_view())
-]
